dffd7a67595c1ae69b2976e1724725d308f7fa4d
khaueviana <khaueviana@multconnect.com.br>
2016-07-24 01:10:10 -0300

    first commit

---
+++ KV.Ef6Migrations/KV.Ef6Migrations.ConsoleApplication/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KV.Ef6Migrations.ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
